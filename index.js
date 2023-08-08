e0ef2df3103191257b65e36d6daedc4896bbe798
shivanireddyp <129300332+shivanireddyp@users.noreply.github.com>
2023-08-08 21:49:08 +0530

    index.js

--- index.js
+++ index.js
@@ -1,62 +1,60 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const registrationForm = document.getElementById("registrationForm");
-    const userList = document.getElementById("userList");
-  
-    registrationForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      const name = registrationForm.name.value;
-      const email = registrationForm.email.value;
-      const password = registrationForm.password.value;
-      const dob = registrationForm.dob.value;
-      const acceptTerms = registrationForm.acceptTerms.checked;
-  
-      // Validate age
-      const today = new Date();
-      const birthDate = new Date(dob);
-      const age = today.getFullYear() - birthDate.getFullYear();
-      if (age < 18 || age > 55) {
-        alert("Age must be between 18 and 55.");
-        return;
-      }
-  
-      if (name && email && password && dob && acceptTerms) {
-        const userInfo = {
-          name,
-          email,
-          password,
-          dob
-        };
-  
-        // Save user info
-        saveUser(userInfo);
-  
-        // Clear form fields
+document.addEventListener('DOMContentLoaded', function () {
+    const registrationForm = document.getElementById('registrationForm');
+    const userTableBody = document.querySelector('#userTable tbody');
+
+    // Load existing entries from localStorage on page load
+    loadEntriesFromLocalStorage();
+
+    registrationForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const dob = document.getElementById('dob').value;
+        const acceptTerms = document.getElementById('acceptTerms').checked;
+
+        if (!acceptTerms) {
+            alert('Please accept the terms.');
+            return;
+        }
+
+        const currentDate = new Date();
+        const dobDate = new Date(dob);
+        const age = currentDate.getFullYear() - dobDate.getFullYear();
+
+        if (age < 18 || age > 55) {
+            alert('Date of birth must be between ages 18 and 55.');
+            return;
+        }
+
+        const newUser = { name, email, password, dob, acceptTerms };
+        addUserToTable(newUser);
+        saveUserEntry(newUser);
+
         registrationForm.reset();
-  
-        // Reload user list
-        loadUsers();
-      }
     });
-  
-    function saveUser(user) {
-      const users = JSON.parse(localStorage.getItem("users")) || [];
-      users.push(user);
-      localStorage.setItem("users", JSON.stringify(users));
+
+    function loadEntriesFromLocalStorage() {
+        const savedEntries = JSON.parse(localStorage.getItem('userEntries')) || [];
+        savedEntries.forEach(entry => addUserToTable(entry));
     }
-  
-    function loadUsers() {
-      userList.innerHTML = "";
-      const users = JSON.parse(localStorage.getItem("users")) || [];
-  
-      users.forEach(user => {
-        const listItem = document.createElement("li");
-        listItem.innerHTML = `<strong>Name:</strong> ${user.name}, <strong>Email:</strong> ${user.email}, <strong>Date of Birth:</strong> ${user.dob}`;
-        userList.appendChild(listItem);
-      });
+
+    function addUserToTable(user) {
+        const newRow = document.createElement('tr');
+        newRow.innerHTML = `
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.password}</td>
+            <td>${user.dob}</td>
+            <td>${user.acceptTerms ? 'Yes' : 'No'}</td>
+        `;
+        userTableBody.appendChild(newRow);
     }
-  
-    // Initial load of user data
-    loadUsers();
-  });
-  
+
+    function saveUserEntry(user) {
+        const savedEntries = JSON.parse(localStorage.getItem('userEntries')) || [];
+        savedEntries.push(user);
+        localStorage.setItem('userEntries', JSON.stringify(savedEntries));
+    }
+});
